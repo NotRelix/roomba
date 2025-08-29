643abb3893a9e2435f04983450917c246b0646f0
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-29 22:42:30 +0800

    feat(types): add user validator type for zod

---
+++ packages/types/src/user.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+
+export const UserValidator = z.object({
+  firstName: z
+    .string()
+    .trim()
+    .min(1, "First name must not be empty")
+    .max(16, "First name must be under 16 characters")
+    .toLowerCase(),
+  lastName: z
+    .string()
+    .trim()
+    .min(1, "Last name must not be empty")
+    .max(16, "Last name must be under 16 characters")
+    .toLowerCase(),
+  username: z
+    .string()
+    .trim()
+    .min(1, "Username must not be empty")
+    .max(16, "Username must be under 16 characters")
+    .toLowerCase(),
+  email: z
+    .email()
+    .trim()
+    .min(1, "Email must not be empty")
+    .max(16, "Email must be under 16 characters"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(255, "Password must be under 255 characters"),
+});
