67a30a6d6a5cc6a292c87854cf7999954a58243a
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-29 22:42:56 +0800

    feat(api): add zValidator to register handler

--- apps/api/src/controllers/register.ts
+++ apps/api/src/controllers/register.ts
@@ -1,10 +1,17 @@
+import { zValidator } from "@hono/zod-validator";
 import { createFactory } from "hono/factory";
+import { UserValidator } from "@repo/types/user"
 
 const factory = createFactory();
 
-export const registerHandler = factory.createHandlers(async (c) => {
-  return c.json({
-    success: true,
-    message: "you've passed by the register handler",
-  });
-});
+export const registerHandler = factory.createHandlers(
+  zValidator("json", UserValidator),
+  async (c) => {
+    const body = await c.req.valid("json");
+    return c.json({
+      success: true,
+      body: body,
+      message: "you've passed by the register handler",
+    });
+  }
+);
