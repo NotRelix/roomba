601865246115ecbfa87e456f83b774b8267b4654
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-31 23:23:56 +0800

    feat(api): add test database for testing

--- apps/api/src/db/drizzle.ts
+++ apps/api/src/db/drizzle.ts
@@ -2,8 +2,13 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
+const databaseUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: databaseUrl,
 });
 
 export const db = drizzle({ client: pool });
--- packages/shared/drizzle.config.ts
+++ packages/shared/drizzle.config.ts
@@ -1,11 +1,16 @@
-import "dotenv";
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
+
+const databaseUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: databaseUrl!,
   },
 });
