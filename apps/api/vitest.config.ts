fe34e844447413f35aa4fc4065d3a03532c7e403
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-09-01 19:28:48 +0800

    refactor(api): remove unnecessary configs for vitest

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    pool: "threads",
-    maxWorkers: 1,
     fileParallelism: false,
-    sequence: {
-      concurrent: false,
-    },
   },
 });
