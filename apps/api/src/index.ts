c9bfc2f70dd2dede72d247712ec7d07b406a87b5
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-09-03 21:21:28 +0800

    refactor(api): separate app listening to test routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,19 +1,8 @@
-import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import auth from "#routes/auth";
 import rooms from "#routes/rooms";
 
-const app = new Hono();
+export const app = new Hono();
 
 app.route("/auth", auth);
 app.route("/rooms", rooms);
-
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  }
-);
