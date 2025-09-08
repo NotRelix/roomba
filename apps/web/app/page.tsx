11e45acf1e3c61772f065a9454e7cefada8f7bdd
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-09-08 22:43:53 +0800

    feat(web): add toggle themes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,15 @@
+import { ModeToggle } from "@/components/toggle-mode";
 import { Button } from "@repo/ui/components/button";
 import { Input } from "@repo/ui/components/input";
 
 export default function Home() {
   return (
     <div className="gap-4 flex">
-      <Button variant={"default"}>no way it worked</Button>
+      <Button className="" variant={"default"}>
+        no way it worked
+      </Button>
       <Input placeholder="thank god ive been doing this for hours"></Input>
+      <ModeToggle />
     </div>
   );
 }
