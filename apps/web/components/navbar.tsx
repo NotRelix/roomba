609aa172f23c062e4bd18852fea06093600018a1
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-09-09 14:51:33 +0800

    style(web): enhance navbar UI with dropdown profile menu

--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
-import { ModeToggle } from "./toggle-mode";
+import { ProfileIcon } from "./profile-icon";
 
 export function Navbar() {
   return (
-    <nav className="sticky top-0 flex justify-between px-4 py-2">
+    <nav className="border-b-1 sticky top-0 flex justify-between border-neutral-200 px-4 py-2 dark:border-neutral-800">
       <Link
         href={"/"}
         className="self-center border-0 text-xl font-semibold ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
       >
         Roomba
       </Link>
-      <ModeToggle />
+      <div className="flex items-center gap-4">
+        <ProfileIcon />
+      </div>
     </nav>
   );
 }
---
+++ apps/web/components/profile-icon.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@repo/ui/components/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@repo/ui/components/dropdown-menu";
+import { UserRound } from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+
+export function ProfileIcon() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          className="border-1 box-content size-4 rounded-lg bg-transparent p-1.5 dark:bg-neutral-800"
+        >
+          <UserRound className="h-4 w-4 dark:stroke-neutral-400" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-24" align="end">
+        <DropdownMenuLabel>Guest</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href={"/login"}>Login</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href={"/register"}>Register</Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Theme</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
